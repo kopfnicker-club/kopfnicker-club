f7b88711cda89b1a8ecf198db3366d0d5bac26d4
Andreas Otten <aotten77@googlemail.com>
2019-03-29 23:33:58 +0100

    add tailwind to project

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 import "./assets/scss/app.scss";
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false;
 
